c43322afe8b77c8bd70dd36bffca7db951ae4feb
Dmitrynk <dmitrynk.kolesnik@gmail.com>
2018-04-06 11:13:47 +0300

    Base commit

---
+++ build/js/data.js
@@ -0,0 +1,42 @@
+
+// $.getJSON('https://test.driventechnical.com/labs/dts/dts_chart/api/v_fetch_miles_transactions_curdate.php', function(data) {
+//     $('#table').bootstrapTable({data: data});
+// })
+
+// $.post('https://test.driventechnical.com/labs/dts/dts_chart/api/fetch_miles_transactions.php', {"fromdate": "20170505", "todate": "20170512", "lane_name": "DFW-NE125"}, function(data) {
+//     $('#table').bootstrapTable({data: data});
+// }, 'json');
+
+
+// $(function() {
+//     var options = {
+//         url: "https://test.driventechnical.com/labs/dts/dts_chart/api/v_fetch_miles_transactions_curdate.php",
+//         columns: [{
+//             field: "id",
+//             title: "ID",
+//             filter: {
+//                 type: "input"
+//             }
+//         }, {
+//             field: "controller",
+//             title: "Controller Plaza",
+//             filter: {
+//                 type: "select",
+//                 data: []
+//             }
+//         }, {
+//             field: "transaction_id",
+//             title: "Transaction",
+//             filter: {
+//                 type: "select",
+//                 data: ["1504622306576001", "1504622286525001", "1504622282034002"]
+//             }
+//         }],
+//         filter: true
+//     };
+//
+//     var $table = $("#table").bootstrapTable(options);
+//     $table.bootstrapTable();
+// });
+
+  /**/
